7f278592e64afd2e0fa6ca4db7ca3292376b4626
wilianbot <wiliamrobaldossantos1@gmail.com>
2025-06-12 11:30:48 -0300

    Adicionado a pagina de login ao admin

---
+++ pages/login.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container class="d-flex justify-center align-center" style="height: 100vh;">
+      <v-card width="400" elevation="8" class="pa-4">
+        <v-card-title class="text-h5 text-center">Login</v-card-title>
+        <v-card-text>
+          <v-text-field
+            label="CPF"
+            type="password"
+            prepend-inner-icon="mdi-lock"
+            variant="outlined"
+            density="comfortable"
+            class="mt-4"
+          />
+          <v-btn color="primary" block class="mt-6" size="large">Entrar</v-btn>
+        </v-card-text>
+  
+        <v-card-actions class="justify-center">
+          <v-btn variant="text" size="small" class="mt-2">Esqueceu a senha?</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-container>
+  </template>
+
+<script>
+export default {
+  name: 'LoginPage'
+}
+</script>
